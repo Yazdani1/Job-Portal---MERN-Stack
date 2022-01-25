95537624b8a10a90f0b0da7ef532eb4f62a94e15
Yazdani <yaz4dani@gmail.com>
2022-01-25 21:05:22 +0200

    job post web view

--- client/src/components/HomePage/Alljobposts.js
+++ client/src/components/HomePage/Alljobposts.js
@@ -1,6 +1,13 @@
-import React from "react";
-
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "../UserContext";
 const Alljobposts = () => {
+
+  //context api
+
+  const [state, setState] = useContext(UserContext);
+  const [loading, setLoading] = useState(true);
+
+
   return (
     <div>
       <h5>All job posts</h5>
--- client/src/components/HomePage/apiHomepage.js
+++ client/src/components/HomePage/apiHomepage.js
@@ -8,4 +8,4 @@ export const getallJobsinhomepage = () => {
       .catch((err) => {
         console.log(err);
       });
-  };
+};
