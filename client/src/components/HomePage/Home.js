60dcd7c048d3e2157acbe8d961fa61d95eca046d
Yazdani <yaz4dani@gmail.com>
2022-01-27 01:38:11 +0200

    footer in home page

--- client/src/components/HomePage/Home.js
+++ client/src/components/HomePage/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Alljobposts from "./Alljobposts";
+import Footer from "./Footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <Alljobposts />
+      <Footer/>
     </div>
   );
 };
