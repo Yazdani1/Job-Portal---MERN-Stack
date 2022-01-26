7b7d85f91d0b586744db7d2e24c157377b5f7356
Yazdani <yaz4dani@gmail.com>
2022-01-27 01:08:46 +0200

    details job design

--- client/src/components/HomePage/Detailsjob/Detailsjob.js
+++ client/src/components/HomePage/Detailsjob/Detailsjob.js
@@ -21,6 +21,10 @@ const Detailsjob = () => {
 
   //details post collapse option state
 
+  const [show, setShow] = useState(true);
+  const [showskills, setShowskills] = useState(true);
+
+
   //loding state
   const [loading, setLoading] = useState(true);
 
@@ -61,22 +65,64 @@ const Detailsjob = () => {
                   {ReactHtmlParser(detailsjob && detailsjob.jobdetails?.des)}
                 </p>
               </div>
+
               <div className="card job-requirements">
                 <div className="job-items-collapse-option">
                   <h6>Requirements</h6>
-                  <p className="collapse-icon-design"><FcCollapse size={20}/></p>
-                </div>
-                <p>
-                  {ReactHtmlParser(
-                    detailsjob && detailsjob.jobdetails?.requirements
+
+                  {show ? (
+                    <p
+                      className="collapse-icon-design"
+                      onClick={() => setShow(!show)}
+                    >
+                      <FcCollapse size={20} />
+                    </p>
+                  ) : (
+                    <p
+                      className="collapse-icon-design"
+                      onClick={() => setShow(!show)}
+                    >
+                      <FcExpand size={20} />
+                    </p>
                   )}
-                </p>
+                </div>
+                {show ? (
+                  <p>
+                    {ReactHtmlParser(
+                      detailsjob && detailsjob.jobdetails?.requirements
+                    )}
+                  </p>
+                ) : null}
               </div>
+
               <div className="card job-skills">
-                <h6>Skills</h6>
-                <p>
-                  {ReactHtmlParser(detailsjob && detailsjob.jobdetails?.skills)}
-                </p>
+                <div className="job-items-collapse-option">
+                  <h6>Skills</h6>
+
+                  {showskills ? (
+                    <p
+                      className="collapse-icon-design"
+                      onClick={() => setShowskills(!showskills)}
+                    >
+                      <FcCollapse size={20} />
+                    </p>
+                  ) : (
+                    <p
+                      className="collapse-icon-design"
+                      onClick={() => setShowskills(!showskills)}
+                    >
+                      <FcExpand size={20} />
+                    </p>
+                  )}
+                </div>
+
+                {showskills ? (
+                  <p>
+                    {ReactHtmlParser(
+                      detailsjob && detailsjob.jobdetails?.skills
+                    )}
+                  </p>
+                ) : null}
               </div>
             </div>
           </div>
