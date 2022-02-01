110d0e17c0172c1c453ef9931a4f0ebebbf68e86
Yazdani <yaz4dani@gmail.com>
2022-02-01 22:52:30 +0200

    all user lists

--- client/src/components/HomePage/AlluserList.js
+++ client/src/components/HomePage/AlluserList.js
@@ -2,25 +2,57 @@ import React, { useEffect, useState } from "react";
 import moment from "moment";
 import { CgProfile } from "react-icons/cg";
 import { Link, useHistory, useParams } from "react-router-dom";
-import Pagination from "../Dashboard/Event/Pagination";
+import Pagination from "../Dashboard/Published Jobs/Pagination";
 import { BsCalendar2DateFill } from "react-icons/bs";
 import "./AlluserList.css";
 import { getallUserlist } from "./apiHomepage";
 
-
 const AlluserList = () => {
- 
-    return (
-        <div className="container">
-          <h6 className="title-user-profile">Visit Event organizers profile</h6>
-          <div className="row">
-            {alluser.map((user, index) => (
+
+
+
+
+  return (
+    <React.Fragment>
+      <div className="container  search-container">
+        <div className="card">
+          <div className="row ">
+            <div className="col-lg-8 col-md-8 col-sm-8 col-xl-8">
+              <div className="eventorganizer-search">
+                <form>
+                  <div className="event-form">
+                    <input
+                      type="text"
+                    //   value={search}
+                    //   onChange={(e) => setSearch(e.target.value)}
+                      className="form-control"
+                      maxLength="100"
+                      placeholder="search event organizers name.."
+                    />
+                  </div>
+                </form>
+                <span>{eventorganizers.length} Users found</span>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-4 col-sm-4 col-xl-4">
+              <div className="eventorganizer-search">
+                <p onClick={searchUser}>Search</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {eventorganizers.length ? (
+            currentUsers.map((user, index) => (
               <div className="col-lg-4 col-md-6 col-sm-6 col-xl-3" key={index}>
                 <div className="user-infocard card">
                   {user && user.photo ? (
                     <div className="user-profile-picture-image">
                       <img src={user && user.photo} />
-    
+
                       <p>{user.name}</p>
                     </div>
                   ) : (
@@ -31,7 +63,7 @@ const AlluserList = () => {
                       <p>{user.name}</p>
                     </div>
                   )}
-    
+
                   <p>{moment(user.createdAt).format("MMMM Do YYYY")}</p>
                   <div className="view-profile-button">
                     <Link
@@ -43,20 +75,32 @@ const AlluserList = () => {
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
-    
-          {alluser ? (
-            <Link to={"/event-organizers"} style={{ textDecoration: "none" }}>
-              <div className="main_container-button">
-                <span className="view-allusers-button">
-                  View All Event Organizers
-                </span>
-              </div>
-            </Link>
+            ))
+          ) : (
+            <h5 className="card">No search result found with your query</h5>
+          )}
+        </div>
+
+        <div className="card pagination-event-organizers">
+          {eventorganizers.length > 1 ? (
+            <Pagination pages={howManyPages} setCurrentPage={setCurrentPage} />
           ) : null}
         </div>
-      );
+        {/* <div className="container">
+            <h5>{star}</h5>
+            <ReactStars
+              count={5}
+              onChange={ratingChanged}
+              size={24}
+              isHalf={true}
+              activeColor="#ffd700"
+            />
+            
+          </div> */}
+      </div>
+      <ToastContainer autoClose={8000} />
+    </React.Fragment>
+  );
 };
 
 export default AlluserList;
