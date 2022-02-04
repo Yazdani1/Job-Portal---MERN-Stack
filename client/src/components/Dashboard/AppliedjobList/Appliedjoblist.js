ccbb38e61744235569f89fa9de9e730319bd3365
Yazdani <yaz4dani@gmail.com>
2022-02-04 18:17:02 +0200

    show all applied jobs

--- client/src/components/Dashboard/AppliedjobList/Appliedjoblist.js
+++ client/src/components/Dashboard/AppliedjobList/Appliedjoblist.js
@@ -2,9 +2,26 @@ import React, { useContext, useEffect, useState } from "react";
 import "./appliedjoblist.css";
 import { getmyappliedjobList } from "./apiAppliedjoblist";
 import ReactHtmlParser from "react-html-parser";
+import Pagination from "../../Dashboard/Published Jobs/Pagination";
+import { FcComboChart, FcFilledFilter } from "react-icons/fc";
+import { EyeOutlined } from "@ant-design/icons";
+import { Link, useHistory, useParams } from "react-router-dom";
+import moment from "moment";
+
 
 const Appliedjoblist = () => {
   const [appliedjoblist, setAppliedjoblist] = useState([]);
+
+    //for pagination
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(8);
+  
+    //Get current posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = appliedjoblist.slice(indexOfFirstPost, indexOfLastPost);
+    const howManyPages = Math.ceil(appliedjoblist.length / postsPerPage);
+  
 
   const loadappliedjoblist = () => {
     getmyappliedjobList()
@@ -24,15 +41,83 @@ const Appliedjoblist = () => {
     <div>
       <h5>Applied job list:</h5>
 
-      {appliedjoblist.map((item,index) => (
-        <>
-          <h4>{index+1}</h4>
-          <p>Job Title: {item.jobpost.name}</p>
-          <p>Job Des: {ReactHtmlParser(item.jobpost.des)}</p>
-          <p>Job Type: {item.jobpost.jobtypes}</p>
+      {/* to show job posts in the table  */}
 
-        </>
-      ))}
+      <div className="container-fluid main_containers">
+        {/* table start */}
+
+        {currentPosts.length > 0 ? (
+          <div className="card table-horizontal">
+            <table class="table table-bordered table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Job Title</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Published on</th>
+                  <th scope="col">Job Types</th>
+                  <th scope="col">Total applications</th>
+
+                  <th scope="col">View Applications</th>
+                  <th colspan="4">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentPosts.map((item, index) => (
+                  <tr key={item._id}>
+                    <th scope="row">{index + 1}</th>
+
+                    <td>{item.jobpost.name.substring(0, 30)}</td>
+                    <td>{ReactHtmlParser(item.jobpost.des?.substring(0, 150))}</td>
+
+                    <td> {moment(item.date).format("MMMM Do YYYY")}</td>
+                    <td>{item.jobtypes}</td>
+
+                    {/* to get all the joined members for each event post */}
+
+                    {/* {item.application.map((joinedmembers) => (
+                      <>
+                        <p>{joinedmembers.name}</p>
+                      </>
+                    ))} */}
+
+                
+
+                    {/* to loops the post comment in the admin dashboard */}
+                    {/* <td>{item.comments.map(c=>(
+                      <h1>{c.text}</h1>
+                    ))}</td> */}
+
+                    <td>
+                      <Link to={"/job-description/" + item.jobpost._id}>
+                        <button className="btn btn-primary">
+                          <EyeOutlined style={{ fontSize: "20px" }} /> View
+                        </button>
+                      </Link>
+                    </td>
+
+                 
+                
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <h5 className="card noposts-design">
+            <FcComboChart size={200} />
+            No data to show!
+          </h5>
+        )}
+
+
+        <div className="card pagination-dashboard">
+          {appliedjoblist.length > 1 ? (
+            <Pagination pages={howManyPages} setCurrentPage={setCurrentPage} />
+          ) : null}
+        </div>
+      </div>
+
     </div>
   );
 };
