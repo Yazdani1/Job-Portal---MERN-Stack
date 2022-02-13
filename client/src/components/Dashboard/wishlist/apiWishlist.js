20846a4032d16f06db766e79f785368966c26541
Yazdani <yaz4dani@gmail.com>
2022-02-13 19:34:09 +0200

    api wishlist

--- client/src/components/Dashboard/wishlist/apiWishlist.js
+++ client/src/components/Dashboard/wishlist/apiWishlist.js
@@ -1,7 +1,6 @@
 //to save job to wishlist
-
 export const savejobtoWishlist = (postID) => {
-  return fetch("/api/create-job-post", {
+  return fetch("/api/job-wishlist", {
     method: "put",
     headers: {
       "Content-Type": "application/json",
@@ -9,6 +8,45 @@ export const savejobtoWishlist = (postID) => {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
     body: JSON.stringify(postID),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+//to remove job from the wishlist
+
+export const removejobfromWishlist = (postID) => {
+  return fetch("/api/remove-job-wishlist", {
+    method: "put",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    body: JSON.stringify(postID),
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+//to get wishlist..
+
+export const getjobWishlist = () => {
+  return fetch("/api/get-job-wishlist", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
   })
     .then((res) => {
       return res.json();
