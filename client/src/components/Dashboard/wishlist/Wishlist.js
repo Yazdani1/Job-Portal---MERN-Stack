30d077d18a0c4f2bedb935b6689aaf254a5bb7a3
Yazdani <yaz4dani@gmail.com>
2022-02-14 20:28:20 +0200

    wishlist design

--- client/src/components/Dashboard/wishlist/Wishlist.js
+++ client/src/components/Dashboard/wishlist/Wishlist.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./wishlist.css";
 import { getjobWishlist } from "./apiWishlist";
+import ReactHtmlParser from "react-html-parser";
 
 const Wishlist = () => {
   const [showwishlist, setShowwishlist] = useState([]);
@@ -26,10 +27,13 @@ const Wishlist = () => {
       <div className="container">
         <div className="row">
           {showwishlist.wishlist?.map((list) => (
-            <div className="col-xl-3">
+            <div className="col-xl-3 col-lg-3 col-md-4">
               <div className="wishlist card" key={list._id}>
                 <h6>{list.name}</h6>
-                <p>{list.des}</p>
+                <p>{ReactHtmlParser(list.des.substring(0, 150))}</p>
+                <div>
+                  
+                </div>
                 <h6>{list.jobtypes}</h6>
               </div>
             </div>
