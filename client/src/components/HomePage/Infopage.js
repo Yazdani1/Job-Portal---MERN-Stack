7c000cf64baee96e878acd87cc8bb8ce37c3032f
Yazdani <yaz4dani@gmail.com>
2022-02-21 15:13:09 +0200

    new page

---
+++ client/src/components/HomePage/Infopage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Infopage = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-lg-3'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Infopage
