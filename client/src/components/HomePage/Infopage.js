3f3e866f260aab779a52c29fe191d79a5f8f6b6b
Yazdani <yaz4dani@gmail.com>
2022-02-21 15:15:19 +0200

    new pages

--- client/src/components/HomePage/Infopage.js
+++ client/src/components/HomePage/Infopage.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Infopage = () => {
   return (
-    <div className='container'>
-        <div className='row'>
-            <div className='col-lg-3'>
-
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3">
+          <h6>Become a member today</h6>
         </div>
+        <div className="col-lg-3">
+          <p>Number of people joined</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Infopage
+export default Infopage;
