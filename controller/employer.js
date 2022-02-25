fdd1b54a08afebea6cdbf63e0884a347adffa4b8
Yazdani <yaz4dani@gmail.com>
2022-02-25 22:04:34 +0200

    employer controller

---
+++ controller/employer.js
@@ -0,0 +1,7 @@
+const Employer = require("../model/Employer");
+
+
+
+
+
+
--- router/employer.js
+++ router/employer.js
@@ -2,10 +2,8 @@ const router = require("express").Router();
 require("dotenv").config();
 const { requireLogin } = require("../middleware/auth");
 
+const { createemployer } = require("../controller/employer");
 
+router.post("/create-employer", requireLogin, createemployer);
 
-
-
-
-
-module.exports = router
+module.exports = router;
