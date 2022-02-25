5b60006c95ee004b1ac9ae2ea41446894cd302db
Yazdani <yaz4dani@gmail.com>
2022-02-25 22:09:16 +0200

    create employer endpoint

--- controller/employer.js
+++ controller/employer.js
@@ -2,4 +2,8 @@ const Employer = require("../model/Employer");
 
 exports.createemployer = (req, res) => {
   const { employername, jobposition, joineddate } = req.body;
+
+  if (!employername) {
+    return res.status(400).json({ error: "Please add your employer name" });
+  }
 };
