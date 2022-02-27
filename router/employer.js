037f4013b9dd3523079d799347610246a0b83ec5
Yazdani <yaz4dani@gmail.com>
2022-02-27 23:18:56 +0200

    delete employer api end point

--- router/employer.js
+++ router/employer.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 require("dotenv").config();
 const { requireLogin } = require("../middleware/auth");
 
-const { createemployer, getemployerList } = require("../controller/employer");
+const {
+  createemployer,
+  getemployerList,
+  deleteEmployer,
+} = require("../controller/employer");
 
 //to create employer list
 router.post("/create-employer", requireLogin, createemployer);
@@ -10,5 +14,9 @@ router.post("/create-employer", requireLogin, createemployer);
 //to get list of employer
 
 router.get("/get-employerlist", requireLogin, getemployerList);
+
+//to delete employer
+
+router.delete("/delete-employer/:id", requireLogin, deleteEmployer);
 
 module.exports = router;
