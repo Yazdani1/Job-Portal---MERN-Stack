2e1e610044d920ac4d638c96f1f17dbc32da1173
Yazdani <yaz4dani@gmail.com>
2022-04-03 22:34:05 +0300

    react pagination

--- client/src/components/Pagination/AddPagination.js
+++ client/src/components/Pagination/AddPagination.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from "react";
 import "./addpagination.css";
+import ReactPaginate from "react-paginate";
 
 const AddPagination = () => {
   return (
-    <div>AddPagination</div>
-  )
-}
+    <React.Fragment>
+      <ReactPaginate
+        breakLabel="..."
+        breakClassName="break-class"
+        nextLabel="Next"
+        pageClassName={"pagination-design"}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        activeClassName={"pagination__link--active"}
+        activeLinkClassName={"active-link-text"}
+        pageCount={pageCount}
+        disabledClassName={"pagination__link--disabled"}
+        previousLabel="Previous"
+        previousClassName={"previous-button"}
+        nextClassName={"next-button"}
+        pageRangeDisplayed={3}
+        pageLinkClassName={"selected-page"}
+      />
+    </React.Fragment>
+  );
+};
 
-export default AddPagination
+export default AddPagination;
