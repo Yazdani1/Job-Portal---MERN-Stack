173bdabcb810bba4319a59abcb4b3e5a34807461
Yazdani <yaz4dani@gmail.com>
2022-04-17 15:20:55 +0300

    react component

---
+++ client/src/components/HomePage/Trending Jobs/Trending.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./trendingjob.css";
+import { FcApproval, FcNightPortrait, FcBookmark } from "react-icons/fc";
+import moment from "moment";
+
+const Trending = ({photo,username,date,name,jobtypes,application}) => {
+  return (
+    <React.Fragment>
+      <div className="trending-job-item">
+        <div className="profile-name-date">
+          {photo ? (
+            <div className="profile-name-avatar-image">
+              <img src={photo} />
+            </div>
+          ) : (
+            <div className="profile-name-avatar">
+              <p>{username?.substring(0, 2).toUpperCase()}</p>
+            </div>
+          )}
+
+          <div className="profile-name-post-date">
+            <p className="profile-name-size">{username}</p>
+          </div>
+
+          <p className="trending">Trending</p>
+        </div>
+        <p>Published on: {moment(date).format("MMM Do YY")}</p>
+
+        <h6>{name.substring(0, 50)}</h6>
+
+        <p className="job-items">
+          <FcApproval size={18} style={{ color: "red" }} />
+          {jobtypes}
+        </p>
+        <p className="job-items">
+          <FcNightPortrait size={20} style={{ color: "red" }} />
+          {application} applicants
+        </p>
+      </div>
+      ;
+    </React.Fragment>
+  );
+};
+
+{
+  /* <div className="trending-job-item">
+  <div className="profile-name-date">
+    {item?.postedBy?.photo ? (
+      <div className="profile-name-avatar-image">
+        <img src={item?.postedBy?.photo} />
+      </div>
+    ) : (
+      <div className="profile-name-avatar">
+        <p>{item.postedBy?.name.substring(0, 2).toUpperCase()}</p>
+      </div>
+    )}
+
+    <div className="profile-name-post-date">
+      <p className="profile-name-size">{item.postedBy?.name}</p>
+    </div>
+
+    <p className="trending">Trending</p>
+  </div>
+  <p>Published on: {moment(item.date).format("MMM Do YY")}</p>
+
+  <h6>{item.name.substring(0, 50)}</h6>
+
+  <p className="job-items">
+    <FcApproval size={18} style={{ color: "red" }} />
+    {item.jobtypes}
+  </p>
+  <p className="job-items">
+    <FcNightPortrait size={20} style={{ color: "red" }} />
+    {item.application.length} applicants
+  </p>
+</div>; */
+}
+
+export default Trending;
--- client/src/components/HomePage/Trending Jobs/TrendingJob.js
+++ client/src/components/HomePage/Trending Jobs/TrendingJob.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./trendingjob.css";
 import { getTrendingjobpost } from "./apiTrending";
+import { FcApproval, FcNightPortrait, FcBookmark } from "react-icons/fc";
+import Trending from "./Trending";
 
 const TrendingJob = () => {
   const [trendingjob, setTrendingjob] = useState([]);
@@ -23,20 +25,19 @@ const TrendingJob = () => {
 
   return (
     <React.Fragment>
-      <div className="container card trending-job">
+      <div className="container-fluid card trending-job">
         <div className="trending-job-title">
           <h2>Trending Jobs</h2>
           <span className="line"></span>
         </div>
-        <div className="row">
-          {trendingjob.map((item, index) => (
-            <div className="col-xl-3 col-lg-3 col-md-3">
-                <div className="trending-job-item">
-                <h6>{item.name}</h6>
-
-                </div>
-            </div>
-          ))}
+        <div className="container">
+          <div className="row">
+            {trendingjob.map((item, index) => (
+              <div className="col-xl-3 col-lg-3 col-md-3">
+                <Trending name={item.name} username={item.postedBy?.name} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </React.Fragment>
